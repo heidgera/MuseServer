46943adbe9c3560b43a904fdbc6a6ab1a2b1b5c0
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2018-10-05 22:03:33 -0500

    first server commit

---
+++ express.js
@@ -0,0 +1,75 @@
+if (!window) var window = global;
+
+var obtains = [
+  'express',
+  'body-parser',
+  'fs',
+  'express-fileupload',
+  'express-session',
+  'https',
+  'http',
+];
+
+obtain(obtains, (express, bodyParser, fs, fileUpload, session, https, http)=> {
+  if (!window.expressServer) {
+    window.expressServer = {};
+    window.expressServer.sessionParser = session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true,
+      cookie: { httpOnly: true, secure: false },
+    });
+    // window.expressServer.sessionParser = session({
+    //   saveUninitialized: false,
+    //   secret: '$eCuRiTy',
+    //   resave: false,
+    // });
+
+    var fileServer = express();
+    var router = express.Router();
+
+    router.use(bodyParser.json());
+    router.use(fileUpload());
+
+    fileServer.use(window.expressServer.sessionParser);
+
+    fileServer.set('view engine', 'pug');
+
+    fileServer.use('', express.static('./client'));
+    fileServer.use('/common', express.static('./common'));
+
+    fileServer.use(router);
+
+    //window.expressServer.httpServer = http.createServer(fileServer).listen(80);
+    var httpApp = fileServer;
+
+    if (muse.useSSL) {
+      const options = {
+        cert: fs.readFileSync(`${global.tld}/sslcert/fullchain.pem`),
+        key: fs.readFileSync(`${global.tld}/sslcert/privkey.pem`),
+      };
+
+      window.expressServer.httpsServer = https.createServer(options, fileServer).listen(443);
+
+      httpApp = function (req, res) {
+        res.writeHead(307, { Location: 'https://' + req.headers['host'] + req.url });
+        res.end();
+      };
+
+    } else window.expressServer.httpsServer = {};
+
+    window.expressServer.httpServer = http.createServer(httpApp).listen(80);
+
+    window.expressServer.fileServer = fileServer;
+    window.expressServer.router = router;
+    window.express = express;
+  }
+
+  exports.fileServer = window.expressServer.fileServer;
+  exports.router = window.expressServer.router;
+  exports.express = window.expressServer.express;
+  exports.httpServer = window.expressServer.httpServer;
+  exports.httpsServer = window.expressServer.httpsServer;
+  exports.sessionParser = window.expressServer.sessionParser;
+
+});
---
+++ wsServer.js
@@ -0,0 +1,113 @@
+if (!window) var window = global;
+
+obtain([`${__dirname}/express.js`, 'ws', 'url', 'µ//events.js'], ({ httpServer, httpsServer, sessionParser }, WebSock, url, { Emitter })=> {
+  if (!window.wsServer) {
+    var manager = new Emitter();
+
+    window.wsServer = new WebSock.Server({
+      verifyClient: (info, done) => {
+        sessionParser(info.req, {}, () => {
+          done(true);
+        });
+      },
+
+      server: (muse.useSSL ? httpsServer : httpServer), });
+    var webSock = null;
+
+    wsServer.broadcast = function (data) {
+      wsServer.clients.forEach(function each(client) {
+        client.send(data);
+      });
+    };
+
+    //wsServer.orderedClients = [];
+
+    wsServer.addListener = (evt, cb)=> {
+      manager.on(evt, cb);
+    };
+
+    // wsServer.addListener = (evt, cb)=> {
+    //   listeners[evt] = cb;
+    // };
+
+    //wsServer.getClient = _id=>wsServer.clients.find(client=>client.id == _id && client.readyState === WebSock.OPEN);
+
+    wsServer.getClient = _id=> {
+      var ret = null;
+      wsServer.clients.forEach(client=> {
+        if (client.id == _id) ret = client;
+      });
+      return ret;
+    };
+
+    wsServer.send = (_id, obj, data)=> {
+      var client = wsServer.getClient(_id);
+      if (client) {
+        if (typeof obj == 'string') obj = { [obj]: data };
+        client.send(JSON.stringify(obj));
+      }
+
+    };
+
+    // var orderedCallbacks = {};
+    //
+    // wsServer.onOrderedConnect = (_id, cb)=> {
+    //   orderedCallbacks[_id] = cb;
+    // };
+
+    wsServer.onConnect;
+
+    wsServer.onClientConnect = (cb)=> {
+      manager.on('clientConnect', cb);
+    };
+
+    wsServer.onClientDisconnect = (cb)=> {
+      manager.on('clientDisconnect', cb);
+    };
+
+    wsServer.on('connection', function (ws, req) {
+      //console.log('client connected');
+      if (!req && ws.upgradeReq) req = ws.upgradeReq;
+
+      ws.id = req.session.id;
+      req.session.ws = ws;
+
+      ws.eventManager = new Emitter();
+      ws.addListener = (evt, cb)=> {
+        ws.eventManager.on(evt, cb);
+      };
+
+      //wsServer.onClientConnect(ws, req);
+      manager.emit('clientConnect', {
+        ws: ws,
+        req: req,
+      });
+
+      ws.on('message', function (message) {
+        var data = JSON.parse(message);
+        for (var key in data) {
+          if (data.hasOwnProperty(key)) {
+            data[key].from = ws.id;
+            manager.emit(key, data[key]);
+            ws.eventManager.emit(key, data[key]);
+          }
+        }
+      });
+
+      ws.on('close', function () {
+        ws.id = null;
+        manager.emit('clientDisconnect', {
+          ws: ws,
+          req: req,
+        });
+      });
+
+      ws.on('error', function (error) {
+      });
+    });
+  }
+
+  exports.wss = wsServer;
+
+  provide(exports);
+});
